24cea826c1bf1090df26a0c0d2b03e1eba3db086
태의 <ted.kordev@gmail.com>
2025-01-19 00:28:39 +0900

    fix : features

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -10,13 +10,41 @@ export const Features = (props) => {
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>{t("features.title")}</h2>
         </div>
-        <div className="row">
+        <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={i} className="col-xs-6 col-md-3">
-                  <i className={d.icon}></i>
-                  <h3>{t(`features.items.${i}.title`)}</h3>
-                  <p>{t(`features.items.${i}.text`)}</p>
+                <div
+                  key={i}
+                  className="col-xs-6 col-md-3"
+                  style={{
+                    marginBottom: "30px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "flex-start",
+                      padding: "0 10px",
+                    }}
+                  >
+                    <i className={d.icon}></i>
+                    <h3>{t(`features.items.${i}.title`)}</h3>
+                    <p
+                      style={{
+                        wordBreak: "keep-all",
+                        wordWrap: "break-word",
+                        flex: 1,
+                      }}
+                    >
+                      {t(`features.items.${i}.text`)}
+                    </p>
+                  </div>
                 </div>
               ))
             : "Loading..."}
